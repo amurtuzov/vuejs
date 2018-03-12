07ca4ed24fec3b338d8a521062e54744dea1a745
Anar <AMurtuzov@RIB.int>
2018-03-12 11:28:13 +0300

    second commit

--- app.js
+++ app.js
@@ -10,14 +10,35 @@ var app = new Vue({
 		getAlbums: function(id) {
 			let url = 'https://jsonplaceholder.typicode.com/albums/?user=' + id;
 			this.getData(url).then(function(response) {
-				Vue.set(this.photographers[id - 1], 'albums', response.data);
+				let albums = [];
+				response.data.forEach((item) => {
+					if(item.userId == id) {
+						albums.push(item);
+					}
+				})
+				Vue.set(this.photographers[id - 1], 'albums', albums);
 			}.bind(this));
 		},
+		// filter: function() {
+		// 	this.photographers.forEach((item) => {
+		// 		item.albums.filter((item) => {
+
+		// 		})
+		// 	})
+		// }
 	},
 	mounted() {
 		axios.get('https://jsonplaceholder.typicode.com/users')
 		.then(function(response) {
 			this.photographers = response.data;
 			}.bind(this));
-	}
+	},
+// 	computed: {
+//   filteredItems: function() {
+//     var self = this;
+//     return this.items.filter(function(item) {
+//       return item.indexOf(self.filter) > -1;
+//     });
+//   }
+// }
 })
